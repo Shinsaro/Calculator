70cdd05d36821b5211d2340b65138575c582642c
Shinsaro <shinsaro69@gmail.com>
2023-09-19 00:13:09 +0700

    looks like calculator, style needs to be improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./styles/calculator.css";
+import * as Icon from "react-bootstrap-icons";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container className="calculator">
+      <Row className="container-form-control">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="0"
+          aria-label="Introduce a number"
+          aria-describedby="basic-addon1"
+        />
+      </Row>
+      <Row className="sectionNumbers">
+        <Col className="number">C</Col>
+        <Col className="number">AC</Col>
+        <Col className="number">%</Col>
+        <Col className="number">/</Col>
+      </Row>
+      <Row className="sectionNumbers">
+        <Col className="number">1</Col>
+        <Col className="number">2</Col>
+        <Col className="number">3</Col>
+        <Col className="number">
+          <Icon.Asterisk style={{ fontSize: "30px" }} />
+        </Col>
+      </Row>
+      <Row className="sectionNumbers">
+        <Col className="number">4</Col>
+        <Col className="number">5</Col>
+        <Col className="number">6</Col>
+        <Col className="number">
+          <Icon.Dash style={{ fontSize: "30px" }} />
+        </Col>
+      </Row>
+      <Row className="sectionNumbers">
+        <Col className="number">7</Col>
+        <Col className="number">8</Col>
+        <Col className="number">9</Col>
+        <Col className="number">
+          <Icon.Plus style={{ fontSize: "30px" }} />
+        </Col>
+      </Row>
+      <Row className="sectionNumbers">
+        <Col className="number0">0</Col>
+        <Col className="number">,</Col>
+        <Col className="number">=</Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default App
+export default App;
